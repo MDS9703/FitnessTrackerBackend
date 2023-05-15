3733df40de15a205d667221e5d385a3be22a8eac
Mind-Rot <cricks312@gmail.com>
2023-05-14 21:57:48 -0600

    Caleb Final routines

--- db/routines.js
+++ db/routines.js
@@ -45,18 +45,83 @@ async function getAllRoutines() {
 }
 
 async function getAllPublicRoutines() {
-  
+  try {
+    const routines = await Routine.find({ isPublic: true }).populate('activities');
+
+    return routines;
+  } catch (error) {
+    throw new Error(`Failed to get all public routines: ${error.message}`);
+  }
 }
 
-async function getAllRoutinesByUser({ username }) {}
+async function getAllRoutinesByUser({ username }) {
+  try {
+    const routines = await Routine.find({ username }).populate('activities');
 
-async function getPublicRoutinesByUser({ username }) {}
+    return routines;
+  } catch (error) {
+    throw new Error(`Failed to get routines for user ${username}: ${error.message}`);
+  }
+}
 
-async function getPublicRoutinesByActivity({ id }) {}
+async function getPublicRoutinesByUser({ username }) {
+  try {
+    const routines = await Routine.find({ username, isPublic: true }).populate('activities');
 
-async function updateRoutine({ id, ...fields }) {}
+    return routines;
+  } catch (error) {
+    throw new Error(`Failed to get public routines for user ${username}: ${error.message}`);
+  }
+}
 
-async function destroyRoutine(id) {}
+async function getPublicRoutinesByActivity({ id }) {
+  try {
+    const routines = await Routine.find({ activity: id, isPublic: true }).populate('activities');
+
+    if (!routines) {
+      throw new Error(`No public routines found for activity with ID ${id}`);
+    }
+
+    return routines;
+  } catch (error) {
+    throw new Error(`Failed to get public routines for activity with ID ${id}: ${error.message}`);
+  }
+}
+
+async function updateRoutine({ id, ...fields }) {
+  try {
+    const routine = await Routine.findById(id).populate('activities');
+
+    if (!routine) {
+      throw new Error(`Routine not found with ID ${id}`);
+    }
+
+    Object.assign(routine, fields);
+
+    const updatedRoutine = await routine.save();
+
+    return updatedRoutine.populate('activities');
+  } catch (error) {
+    throw new Error(`Failed to update routine with ID ${id}: ${error.message}`);
+  }
+}
+
+async function destroyRoutine(id) {
+  try {
+    const routine = await Routine.findById(id).populate('activities');
+
+    if (!routine) {
+      throw new Error('Routine not found');
+    }
+
+    await Routine.deleteOne({ _id: id });
+
+    const allRoutines = await Routine.find().populate('activities');
+    return allRoutines;
+  } catch (error) {
+    throw new Error(`Failed to destroy routine with ID ${id}: ${error.message}`);
+  }
+}
 
 module.exports = {
   getRoutineById,
